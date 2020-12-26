b1c72d503fa97ad667dc5b14930b07a3f9957e10
Pratik <pratik@ghanshyamdigital.com>
2020-12-26 17:45:01 +0530

    set data in localstorage.

--- src/app/main/apps/property/property.js
+++ src/app/main/apps/property/property.js
@@ -1,32 +1,36 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Select from 'react-select';
 import './property.css'
 import {useHistory} from 'react-router-dom'
+
 function Property() {
     const history = useHistory();
-    const initialValues = {
-        propertyValue: "",
-        downPayment: "",
-        downPaymentPercentage: "",
-    };
+    // const initialValues = {
+    //     propertyValue: "",
+    //     downPayment: "",
+    //     downPaymentPercentage: "",
+    //     selectPurpose: "",
+    //     selectProperty: "",
+    // };
     const selectPurpose = [
-        {value: 'purpose', label: 'Purpose'},
-        {value: 'blue', label: 'Blue'},
-        {value: 'purple', label: 'Purple'},
-        {value: 'red', label: 'Red'},
-        {value: 'orange', label: 'Orange'},
-        {value: 'yellow', label: 'Yellow'},
-        {value: 'green', label: 'Green'},
-        {value: 'forest', label: 'Forest'},
-        {value: 'slate', label: 'Slate'},
-        {value: 'silver', label: 'Silver'},
+        {value: 0, label: '1'},
+        {value: 1, label: '2'},
+        {value: 2, label: '3'},
     ]
-
-
+    const cachedHits = localStorage.getItem('propertyValue');
+    const [initialValues, setInitialValues] = React.useState(
+        JSON.parse(cachedHits) || {
+            propertyValue: "",
+            downPayment: "",
+            downPaymentPercentage: "",
+            selectPurpose: "",
+            selectProperty: "",
+        }
+    );
     const selectProperty = [
-        {value: 'SingleFamilyHouse', label: 'Single Family House'},
-        {value: 'MultiFamilyHouse', label: 'Multi Family House'},
-        {value: 'Condominium', label: 'Condominium'}
+        {value: 0, label: 'Single Family House'},
+        {value: 1, label: 'Multi Family House'},
+        {value: 2, label: 'Condominium'}
     ]
 
     const [formValues, setFormValues] = useState(initialValues);
@@ -35,11 +39,14 @@ function Property() {
 
     useEffect(() => {
         if (Object.keys(formErrors).length === 0 && isSubmitting) {
+            localStorage.setItem('propertyValue', JSON.stringify(formValues));
             submitForm();
         }
     }, [formErrors]);
 
-    const submitForm = useCallback(() => history.push('/financialDetail'), [history]);
+    const submitForm = useCallback(() => {
+        history.push('/financialDetail')
+    }, [history]);
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -61,21 +68,21 @@ function Property() {
     const validate = (values) => {
         let errors = {};
         if (!values.propertyValue) {
-            errors.propertyValue = "Required field!";
+            errors.propertyValue = "Property field is required!";
         } else if (values.propertyValue >= 10000000000) {
             errors.propertyValue = "property Value must be $10000000000";
         }
 
         if (!values.downPayment) {
-            errors.downPayment = "Required field!";
+            errors.downPayment = "DownPayment field is required!";
         } else if (values.downPayment > values.propertyValue) {
-            errors.downPayment = "Invalid DownPayment";
+            errors.downPayment = "The downPayment should be less than the property!";
         }
 
         if (!values.downPaymentPercentage) {
-            errors.downPaymentPercentage = "Required field!";
+            errors.downPaymentPercentage = "Percentage field is required!";
         } else if (values.downPaymentPercentage > 100) {
-            errors.downPaymentPercentage = "Enter valid percentage";
+            errors.downPaymentPercentage = "The value of Percentage should be 100 or less";
         }
         return errors;
     };
@@ -83,9 +90,6 @@ function Property() {
     return (
         <>
             <div className="col-md-12 bg-white" style={{height: '87vh'}}>
-                <h3>"These are Property details."</h3>
-                <hr/>
-
                 <form onSubmit={handleSubmit} noValidate>
                     <div className="row">
                         <div className="col-md-4 mt-4 mb-2">
@@ -141,7 +145,7 @@ function Property() {
                             <Select
                                 className="basic-single"
                                 classNamePrefix="select"
-                                defaultValue={selectPurpose[0]}
+                                defaultValue={formValues.selectPurpose ? selectPurpose[formValues.selectPurpose] : selectPurpose[0]}
                                 isDisabled={false}
                                 isLoading={false}
                                 isClearable={false}
@@ -158,7 +162,7 @@ function Property() {
                             <Select
                                 className="basic-single"
                                 classNamePrefix="select"
-                                defaultValue={selectProperty[0]}
+                                defaultValue={formValues.selectProperty ? selectProperty[formValues.selectProperty] : selectProperty[0]}
                                 isDisabled={false}
                                 isLoading={false}
                                 isClearable={false}
